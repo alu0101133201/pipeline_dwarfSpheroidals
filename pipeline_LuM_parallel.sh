daa9f1825a2962d9e2f87421f032e6f6634db592
Sergio Guerra Arencibia <sguerra@pugna.dyn.iac.es>
2024-07-24 14:09:06 +0100

    changed the keyword of number of frames. Now takes into account the frames lost in the reduction

--- pipeline_LuM_parallel.sh
+++ pipeline_LuM_parallel.sh
@@ -1121,8 +1121,9 @@ fi
 
 
 # Useful keywords for the final image
+numberOfFramesUsed=$(ls $mowdir/*.fits | wc -l)
 writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "NIGHTSCOMBINED" $numberOfNights
-writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $totalNumberOfFrames
+writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $numberOfFramesUsed
 
 # # --- Build exposure map
 # exposuremapDir=$baseCoaddir/exposureMap
@@ -1266,8 +1267,9 @@ else
 fi
 
 # Useful keywords for the final image
+numberOfFramesUsed=$(ls $mowdir/*.fits | wc -l)
 writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "NIGHTSCOMBINED" $numberOfNights
-writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $totalNumberOfFrames
+writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $numberOfFramesUsed
 
 # Remove intermediate folders to save some space
 find $BDIR/sub-sky-fullGrid_it2 -type f ! -name 'done*' -exec rm {} \;
@@ -1381,9 +1383,9 @@ baseCoaddir=$BDIR/coadds_it$iteration
 buildCoadd $baseCoaddir $mowdir $moonwdir
 
 # Useful keywords for the final image
+numberOfFramesUsed=$(ls $mowdir/*.fits | wc -l)
 writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "NIGHTSCOMBINED" $numberOfNights
-writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $totalNumberOfFrames
-
+writeKeywordToFits $baseCoaddir/stdWeighted/"$objectName"_coadd1_"$filter".fits 1 "FRAMESCOMBINED" $numberOfFramesUsed
 
 
 
