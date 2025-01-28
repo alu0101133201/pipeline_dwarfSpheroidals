d589217351ae812726bd1265867ea43a49dc3005
Ignacio Ruiz <ignacio.ruiz@iac.es>
2025-01-28 16:53:52 +0000

    Advances of addapting into multiple detector: checked up to it1 of flat field. Added a script to convert into a single flat ring into different flat rings needed for multiple detectors.

---
+++ pipelineScripts/singleCameratoCCD_cordtransform.py
@@ -0,0 +1,17 @@
+import sys
+from astropy.io import fits
+from astropy.wcs import WCS
+import os
+
+image=sys.argv[1]
+ccd=int(sys.argv[2])
+x_camera=float(sys.argv[3])
+y_camera=float(sys.argv[4])
+
+image_data=fits.open(image)
+image_wcs=WCS(image_data[ccd].header)
+
+x_detector,y_detector=image_wcs.world_to_pixel_values(x_camera,y_camera)
+
+print(f"x_det={x_detector}")
+print(f"y_det={y_detector}")
